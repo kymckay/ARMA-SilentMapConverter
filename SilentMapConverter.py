46f39002d5bcd328293cfeda78b2f2bdf0199168
SilentSpike <SilentSpike100+Github@gmail.com>
2014-10-19 17:34:59 +0100

    Added support for multiple sqm files
    Will convert all sqm files in the directory

--- SilentMapConverter.py
+++ SilentMapConverter.py
@@ -14,136 +14,137 @@ versionNum = 1.0
 import os
 import re
 
-#Path to mission.sqm required for reading
-#Mission file should be in same directroy as script
-scriptDirectory = os.path.dirname(os.path.realpath(__file__)) + "\\"
-missionPath = scriptDirectory + "mission.sqm"
+#Path to .sqm files required for reading
+#Mission files should be in same directroy as script
+scriptDirectory = os.path.dirname(os.path.realpath(__file__))
+sqmFiles = []
+for fileName in os.listdir(scriptDirectory):
+    if fileName[len(fileName)-4:] == ".sqm":
+        sqmFiles.append(fileName)
+scriptDirectory += "\\"
 
-#Make sure the file exists before opening to avoid errors
-if os.path.isfile(missionPath):
-    #Compile the frequently used reset regular expressions for efficency
-    reReset = re.compile(r"\s;\n")
-    reSubReset = re.compile(r"\s{2}\};\n")
+for fileName in sqmFiles:
+    missionPath = scriptDirectory + fileName
+    #Make sure the file exists before opening to avoid errors
+    if os.path.isfile(missionPath):
+        #Compile the frequently used reset regular expressions for efficency
+        reReset = re.compile(r"\s;\n")
+        reSubReset = re.compile(r"\s{2}\};\n")
 
-    #Loop through each line in the mission file for omtimization
-    with open(missionPath) as missionFile:
-        outputString = "// Created by SMC v{0}\n\n".format(versionNum)
-        subScope = "" #Will temporarily store each group
-        mode = 0 #Mode will refer to the current config scope 0-None, 1-Groups
-        for line in missionFile:
-            if mode == 0:
-                if re.search(r"\sclass Groups\n",line,re.I):
-                    mode = 1
-                    outputString += "// ### GROUPS ###\n_westHQ = createCenter west;\n_eastHQ = createCenter east;\n_guerHQ = createCenter resistance;\n_civHQ  = createCenter civilian;\n"
-            elif mode == 1:
-                #The current group can be processed when the end of it is reached
-                if reSubReset.match(line):
-                    groupSide = re.search(r"\s{3}side=\"(\w+?)\";",subScope,re.I)
-                    itemsArray = re.findall(r"\s{4}class Item\d+\n\s{4}\{.+?\s{4}\};",subScope,re.I|re.S)
+        #Loop through each line in the mission file for omtimization
+        with open(missionPath) as missionFile:
+            outputString = "// Created by SMC v{0}\n\n".format(versionNum)
+            subScope = "" #Will temporarily store each group
+            mode = 0 #Mode will refer to the current config scope 0-None, 1-Groups
+            for line in missionFile:
+                if mode == 0:
+                    if re.search(r"\sclass Groups\n",line,re.I):
+                        mode = 1
+                        outputString += "// ### GROUPS ###\n_westHQ = createCenter west;\n_eastHQ = createCenter east;\n_guerHQ = createCenter resistance;\n_civHQ  = createCenter civilian;\n"
+                elif mode == 1:
+                    #The current group can be processed when the end of it is reached
+                    if reSubReset.match(line):
+                        groupSide = re.search(r"\s{3}side=\"(\w+?)\";",subScope,re.I)
+                        itemsArray = re.findall(r"\s{4}class Item\d+\n\s{4}\{.+?\s{4}\};",subScope,re.I|re.S)
 
-                    #Correctly sided group must be created before units can be
-                    if groupSide:
-                        if groupSide.group(1) == "CIV":
-                            groupSide = "civilian"
-                        elif groupSide.group(1) == "GUER":
-                            groupSide = "resistance"
-                        else:
-                            groupSide = groupSide.group(1).lower()
-                        outputString += "\n_currentGroup = createGroup " + groupSide + ";\n"
-
-                    #Units and waypoints can now be created
-                    for currentItem in itemsArray:
-                        #Only waypint classes contain an effects class
-                        if re.search(r"\s{5}class Effects",currentItem,re.I):
-                            #itemDetails = re.search()
-                            outputString += "Waypoint\n"
-                        else:
-                            #Condition and probability of presence should be checked first before unit is created
-                            vehPresence = re.search(r"presenceCondition=\"(.+)\";",currentItem,re.I)
-                            vehProbability = re.search(r"presence=(\d\.?\d*);",currentItem,re.I)
-                            if vehPresence and vehProbability:
-                                outputString += "if (({0}) and (random 1 < {1})) then {2}\n\t".format(vehPresence.group(1),vehProbability.group(1)[:4],"{")
-                            elif vehPresence and not vehPresence:
-                                outputString += "if ({0}) then {1}\n\t".format(vehPresence.group(1),"{")
-                            elif vehProbability and not vehPresence:
-                                outputString += "if (random 1 < {0}) then {1}\n\t".format(vehProbability.group(1)[:4],"{")
-
-                            #Use variable name if it exists - instead of a local variable
-                            vehVarName = re.search(r"text=\"(.+)\";",currentItem,re.I)
-                            if vehVarName:
-                                vehVarName = vehVarName.group(1)
+                        #Correctly sided group must be created before units can be
+                        if groupSide:
+                            if groupSide.group(1) == "CIV":
+                                groupSide = "civilian"
+                            elif groupSide.group(1) == "GUER":
+                                groupSide = "resistance"
                             else:
-                                vehVarName = "_currentUnit"
+                                groupSide = groupSide.group(1).lower()
+                            outputString += "\n_currentGroup = createGroup " + groupSide + ";\n"
 
-                            #Classname, position, placement radius and special attributes are all needed for unit creation
-                            vehName = re.search(r"vehicle=(\".+\");",currentItem,re.I).group(1)
-                            vehPosition = "[" + ",".join(re.search(r"position\[\]=\{(\d+\.?\d*),.+,(\d+\.?\d*)\};",currentItem,re.I).group(1,2)) + ",0]"
-                            vehRadius = re.search(r"placement=(\d+\.?\d*);",currentItem,re.I)
-                            if vehRadius:
-                                vehRadius = vehRadius.group(1)
+                        #Units and waypoints can now be created
+                        for currentItem in itemsArray:
+                            #Only waypint classes contain an effects class
+                            if re.search(r"\s{5}class Effects",currentItem,re.I):
+                                #itemDetails = re.search()
+                                outputString += "Waypoint\n"
                             else:
-                                vehRadius = "0"
-                            vehSpecial = re.search(r"special=(\".*\");",currentItem,re.I)
-                            if vehSpecial:
-                                vehSpecial = vehSpecial.group(1)
-                            else:
-                                vehSpecial = "\"FORM\""
-                            outputString += "{0} = _currentGroup createUnit [{1}];\n".format(vehVarName,",".join([vehName,vehPosition,"[]",vehRadius,vehSpecial]))
+                                #Condition and probability of presence should be checked first before unit is created
+                                vehPresence = re.search(r"presenceCondition=\"(.+)\";",currentItem,re.I)
+                                vehProbability = re.search(r"presence=(\d\.?\d*);",currentItem,re.I)
+                                if vehPresence and vehProbability:
+                                    outputString += "if (({0}) and (random 1 < {1})) then {2}\n\t".format(vehPresence.group(1),vehProbability.group(1)[:4],"{")
+                                elif vehPresence and not vehPresence:
+                                    outputString += "if ({0}) then {1}\n\t".format(vehPresence.group(1),"{")
+                                elif vehProbability and not vehPresence:
+                                    outputString += "if (random 1 < {0}) then {1}\n\t".format(vehProbability.group(1)[:4],"{")
 
-                            #Vehicles won't work with createUnit so this workaround is needed
-                            for appendString in ["if !(alive {0}) then {1}\n".format(vehVarName,"{"),"\t{0} = createVehicle [{1}];\n".format(vehVarName,",".join([vehName,vehPosition,"[]",vehRadius,vehSpecial])),"\t[{0},_currentGroup] call BIS_fnc_spawnCrew;\n".format(vehVarName),"};\n"]:
+                                #Use variable name if it exists - instead of a local variable
+                                vehVarName = re.search(r"text=\"(.+)\";",currentItem,re.I)
+                                if vehVarName:
+                                    vehVarName = vehVarName.group(1)
+                                else:
+                                    vehVarName = "_currentUnit"
+
+                                #Classname, position, placement radius and special attributes are all needed for unit creation
+                                vehName = re.search(r"vehicle=(\".+\");",currentItem,re.I).group(1)
+                                vehPosition = "[" + ",".join(re.search(r"position\[\]=\{(\d+\.?\d*),.+,(\d+\.?\d*)\};",currentItem,re.I).group(1,2)) + ",0]"
+                                vehRadius = re.search(r"placement=(\d+\.?\d*);",currentItem,re.I)
+                                if vehRadius:
+                                    vehRadius = vehRadius.group(1)
+                                else:
+                                    vehRadius = "0"
+                                vehSpecial = re.search(r"special=(\".*\");",currentItem,re.I)
+                                if vehSpecial:
+                                    vehSpecial = vehSpecial.group(1)
+                                else:
+                                    vehSpecial = "\"FORM\""
+                                outputString += "{0} = _currentGroup createUnit [{1}];\n".format(vehVarName,",".join([vehName,vehPosition,"[]",vehRadius,vehSpecial]))
+
+                                #Vehicles won't work with createUnit so this workaround is needed
+                                for appendString in ["if !(alive {0}) then {1}\n".format(vehVarName,"{"),"\t{0} = createVehicle [{1}];\n".format(vehVarName,",".join([vehName,vehPosition,"[]",vehRadius,vehSpecial])),"\t[{0},_currentGroup] call BIS_fnc_spawnCrew;\n".format(vehVarName),"};\n"]:
+                                    if vehPresence or vehProbability:
+                                        outputString += "\t"
+                                    outputString += appendString
+
+                                #Set unit direction if necessary
+                                vehDir = re.search(r"azimut=(\d+\.?\d*);",currentItem,re.I)
+                                if vehDir:
+                                    if vehPresence or vehProbability:
+                                        outputString += "\t"
+                                    outputString += "{0} setDir {1};\n".format(vehVarName,vehDir.group(1))
+
+                                #Set health, fuel, skill and ammo only if necessary
+                                vehHealth = re.search(r"health=(\d+\.?\d*);",currentItem,re.I)
+                                vehFuel = re.search(r"fuel=(\d+\.?\d*);",currentItem,re.I)
+                                vehSkill = re.search(r"skill=(\d+\.?\d*);",currentItem,re.I)
+                                vehAmmo = re.search(r"ammo=(\d+\.?\d*);",currentItem,re.I)
+                                if vehHealth:
+                                    if vehPresence or vehProbability:
+                                        outputString += "\t"
+                                    outputString += "{0} setDamage {1};\n".format(vehVarName,str(1 - float(vehHealth.group(1)))[:4])
+                                if vehFuel:
+                                    if vehPresence or vehProbability:
+                                        outputString += "\t"
+                                    outputString += "{0} setFuel {1};\n".format(vehVarName,vehFuel.group(1)[:4])
+                                if vehSkill.group(1) != "0.60000002":
+                                    if vehPresence or vehProbability:
+                                        outputString += "\t"
+                                    outputString += "{0} setSkill {1};\n".format(vehVarName,vehSkill.group(1)[:4])
+                                if vehAmmo:
+                                    if vehPresence or vehProbability:
+                                        outputString += "\t"
+                                    outputString += "{0} setVehicleAmmo {1};\n".format(vehVarName,vehAmmo.group(1)[:4])
+
+                                #If any condition of presence is present then the if statement must be closed
                                 if vehPresence or vehProbability:
-                                    outputString += "\t"
-                                outputString += appendString
+                                    outputString += "};\n"
 
-                            #Set unit direction if necessary
-                            vehDir = re.search(r"azimut=(\d+\.?\d*);",currentItem,re.I)
-                            if vehDir:
-                                if vehPresence or vehProbability:
-                                    outputString += "\t"
-                                outputString += "{0} setDir {1};\n".format(vehVarName,vehDir.group(1))
+                        subScope = ""
+                    else:
+                        subScope += line
+                #Mode should be set back to 0 when current config scope is exited
+                if mode != 0 and reReset.match(line):
+                    mode = 0
 
-                            #Set health, fuel, skill and ammo only if necessary
-                            vehHealth = re.search(r"health=(\d+\.?\d*);",currentItem,re.I)
-                            vehFuel = re.search(r"fuel=(\d+\.?\d*);",currentItem,re.I)
-                            vehSkill = re.search(r"skill=(\d+\.?\d*);",currentItem,re.I)
-                            vehAmmo = re.search(r"ammo=(\d+\.?\d*);",currentItem,re.I)
-                            if vehHealth:
-                                if vehPresence or vehProbability:
-                                    outputString += "\t"
-                                outputString += "{0} setDamage {1};\n".format(vehVarName,str(1 - float(vehHealth.group(1)))[:4])
-                            if vehFuel:
-                                if vehPresence or vehProbability:
-                                    outputString += "\t"
-                                outputString += "{0} setFuel {1};\n".format(vehVarName,vehFuel.group(1)[:4])
-                            if vehSkill.group(1) != "0.60000002":
-                                if vehPresence or vehProbability:
-                                    outputString += "\t"
-                                outputString += "{0} setSkill {1};\n".format(vehVarName,vehSkill.group(1)[:4])
-                            if vehAmmo:
-                                if vehPresence or vehProbability:
-                                    outputString += "\t"
-                                outputString += "{0} setVehicleAmmo {1};\n".format(vehVarName,vehAmmo.group(1)[:4])
-
-                            #If any condition of presence is present then the if statement must be closed
-                            if vehPresence or vehProbability:
-                                outputString += "};\n"
-
-                    subScope = ""
-                else:
-                    subScope += line
-            #Mode should be set back to 0 when current config scope is exited
-            if mode != 0 and reReset.match(line):
-                mode = 0
-
-    #Avoid issue by sanitizing the user's chosen filename
-    outputFilename = raw_input("Enter a filename for the script you wish to create:")
-    if (outputFilename[len(outputFilename) - 4:]) != ".sqf":
-        outputFilename += ".sqf"
-
-    #The written file should be created/overwritten in the same directory
-    outputPath = scriptDirectory + outputFilename
-    with open(outputPath, 'w') as outputFile:
-        outputFile.write(outputString)
-else:
-    print "There is no mission.sqm in this directory"
+        #The written file should be created/overwritten in the same directory
+        outputPath = scriptDirectory + fileName[:len(fileName)-1] + "f"
+        with open(outputPath, 'w') as outputFile:
+            outputFile.write(outputString)
+    else:
+        print "There is no mission.sqm in this directory"
---
+++ TestingMission.sqf
@@ -0,0 +1,50 @@
+// Created by SMC v1.0
+
+// ### GROUPS ###
+_westHQ = createCenter west;
+_eastHQ = createCenter east;
+_guerHQ = createCenter resistance;
+_civHQ  = createCenter civilian;
+
+_currentGroup = createGroup west;
+if ((true and not(false)) and (random 1 < 0.4)) then {
+	HereIsAName = _currentGroup createUnit ["CZ_Special_Forces_Scout_DES_EP1",[990.29712,1434.9325,0],[],300,"FORM"];
+	if !(alive HereIsAName) then {
+		HereIsAName = createVehicle ["CZ_Special_Forces_Scout_DES_EP1",[990.29712,1434.9325,0],[],300,"FORM"];
+		[HereIsAName,_currentGroup] call BIS_fnc_spawnCrew;
+	};
+	HereIsAName setDir 30;
+	HereIsAName setDamage 0.44;
+	HereIsAName setFuel 0.66;
+	HereIsAName setSkill 0.48;
+	HereIsAName setVehicleAmmo 0.36;
+};
+
+_currentGroup = createGroup east;
+_currentUnit = _currentGroup createUnit ["TK_INS_Soldier_EP1",[988.47803,1435.2903,0],[],0,"FORM"];
+if !(alive _currentUnit) then {
+	_currentUnit = createVehicle ["TK_INS_Soldier_EP1",[988.47803,1435.2903,0],[],0,"FORM"];
+	[_currentUnit,_currentGroup] call BIS_fnc_spawnCrew;
+};
+
+_currentGroup = createGroup resistance;
+_currentUnit = _currentGroup createUnit ["CIV_Contractor1_BAF",[988.47803,1433.4713,0],[],0,"CARGO"];
+if !(alive _currentUnit) then {
+	_currentUnit = createVehicle ["CIV_Contractor1_BAF",[988.47803,1433.4713,0],[],0,"CARGO"];
+	[_currentUnit,_currentGroup] call BIS_fnc_spawnCrew;
+};
+
+_currentGroup = createGroup civilian;
+_currentUnit = _currentGroup createUnit ["Citizen1",[986.33081,1434.9624,0],[],0,"FORM"];
+if !(alive _currentUnit) then {
+	_currentUnit = createVehicle ["Citizen1",[986.33081,1434.9624,0],[],0,"FORM"];
+	[_currentUnit,_currentGroup] call BIS_fnc_spawnCrew;
+};
+_currentUnit = _currentGroup createUnit ["Citizen1",[985.55548,1434.9028,0],[],0,"FORM"];
+if !(alive _currentUnit) then {
+	_currentUnit = createVehicle ["Citizen1",[985.55548,1434.9028,0],[],0,"FORM"];
+	[_currentUnit,_currentGroup] call BIS_fnc_spawnCrew;
+};
+Waypoint
+Waypoint
+Waypoint
