598533f5d031e1d2df2daf009fd27ae4a59c07a3
SilentSpike <silentspike100+Github@gmail.com>
2015-02-13 17:12:38 +0000

    Removed DPs from rounding

--- SilentMapConverter.py
+++ SilentMapConverter.py
@@ -95,7 +95,7 @@ def procGroups(groupsList):
                         #Make a list of coordinates as strings
                         unitPos = unitPos.group(1).split(",")
                         #Round all coordinates to 2 DP then convert to string
-                        unitPos = ",".join([str(round(float(i),2)) for i in unitPos])
+                        unitPos = ",".join([str(int(round(float(i)))) for i in unitPos])
                         returnCode += "{0} = _group{1} createUnit [\"{2}\",[{3}],[],0,\"\"];\n".format(unitVariable,groupIndex,unitType,unitPos)
                     else:
                         return malformed("unit {0} in group {1} has no position".format(unitIndex,groupIndex))
